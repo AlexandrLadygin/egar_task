af7e6dced1cb8f00d3d082b9398effefcb8d2641
Aliaksandr Ladyhin <aliaksandr_ladyhin@epam.com>
2021-04-18 19:46:30 +0300

    add Log

---
+++ src/main/java/by/zeon/loggers/Log.java
@@ -0,0 +1,12 @@
+package by.zeon.loggers;
+
+import java.util.logging.Logger;
+
+public class Log {
+    private static java.util.logging.Logger LOGGER = Logger.getLogger("custom-logger");
+
+    public static void info(String message) {
+        LOGGER.info(message);
+    }
+
+}
