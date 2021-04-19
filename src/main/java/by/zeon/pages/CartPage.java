1a2a0ee98a3b80dbae54a9809b2b06b517e4e514
Aliaksandr Ladyhin <aliaksandr_ladyhin@epam.com>
2021-04-19 13:08:13 +0300

    add new method openPage() to CartPage

--- src/main/java/by/zeon/pages/CartPage.java
+++ src/main/java/by/zeon/pages/CartPage.java
@@ -4,8 +4,10 @@ import by.zeon.loggers.Log;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class CartPage extends BasePage {
+    private static final String CART_PAGE_URL = "/cart.php?id";
     private static final String PRODUCT_NAME_PATH = "//div[@class='cart-items']" +
             "//div[@class='cart-item flex valign-center']" +
             "//div[@class='cart-item-product-title']/a[contains(text(),'%s')]";
@@ -24,5 +26,12 @@ public class CartPage extends BasePage {
         String resultPrice = $(By.xpath(String.format(PRODUCT_PRICE_PATH, name))).getText();
         Log.info(String.format("Text from product price: '%s'.", resultPrice));
         return resultPrice;
+    }
+
+    public CartPage openPage() {
+        String cartPageUrl = String.format(BASE_URL.concat(CART_PAGE_URL));
+        open(cartPageUrl);
+        Log.info(String.format("Open Cart Page: '%s'.", cartPageUrl));
+        return this;
     }
 }
